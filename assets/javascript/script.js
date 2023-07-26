6b1717820452e50ad44e9c3795d0d4ade8da7cf5
hitesh suthar <hite.sh10338@gmail.com>
2023-07-26 16:13:12 +0530

    420

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,16 @@ function addtocart(item) {
     selectedItem.setAttribute('id',addItemId);
     var img = document.createElement('img');
     img.setAttribute('src',item.children[0].currentSrc);
+    var title = document.createElement('div');
+    title.innerText = item.children[1].innerText;
+    var label = document.createElement('div');
+    label.innerText = item.children[2].children[0].innerText;
+    var select = document.createElement('span');
+    select.innerText = item.children[2].children[1].value;
+    label.append(select);
     var cartItems = document.getElementById('product-title');
     selectedItem.append(img);
+    selectedItem.append(title);
     cartItems.append(selectedItem);
+
 }
