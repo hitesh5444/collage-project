2c310cb03ad92173c7866514451ee94264b6b41f
hitesh suthar <hite.sh10338@gmail.com>
2023-07-26 19:50:05 +0530

    1

--- assets/javascript/ofline.js
+++ assets/javascript/ofline.js
@@ -1,4 +1,6 @@
-  function ofline() {
-    document.getElementById("light").classList.toggle("ofline");
-    document.getElementById("lightblink").classList.toggle("ofline");
-  }
+const myTimeout = setTimeout(myGreeting, 10000);
+
+function myGreeting() {
+  document.getElementById("light").classList.add("ofline");
+  document.getElementById("lightblink").classList.add("ofline");
+}
